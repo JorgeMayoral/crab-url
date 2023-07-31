5c6b7aeaf53f69c9d9fb77ae7905ba9e4c94793a
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2023-07-31 17:34:24 +0200

    feat(backend): add simple server with health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router, Server};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health", get(health_check));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {addr}");
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server");
+}
+
+async fn health_check() -> &'static str {
+    "OK"
 }
