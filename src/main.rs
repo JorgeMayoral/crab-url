af0dffe1deb0e7a3dc278f333431e23f165bb9e7
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2023-08-03 16:02:55 +0200

    feat: return id of added url

--- src/main.rs
+++ src/main.rs
@@ -88,22 +88,25 @@ struct AddUrlBody {
 async fn add_url(
     State(app_state): State<Arc<AppState>>,
     Json(body): Json<AddUrlBody>,
-) -> StatusCode {
+) -> (StatusCode, String) {
     let connection = app_state.redis_client.get_connection();
     if let Err(connection) = connection {
         tracing::error!(
             "Failed to connect to redis in add url. Error: {}",
             connection
         );
-        return StatusCode::INTERNAL_SERVER_ERROR;
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Failed to connect to redis".to_string(),
+        );
     }
     let mut connection = connection.unwrap();
     let ttl = 6 * 60 * 60; // 6 hours
     let id = nanoid::nanoid!(5);
     let url = body.url;
     tracing::debug!("Adding url: {url} with id: {id}");
-    let _: Result<String, redis::RedisError> = connection.set_ex(id, url, ttl);
-    StatusCode::OK
+    let _: Result<String, redis::RedisError> = connection.set_ex(&id, url, ttl);
+    (StatusCode::OK, id)
 }
 
 async fn not_found() -> &'static str {
