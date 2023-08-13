12eaced1d92bce430d2fb2e21f02f18fede31bfd
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2023-08-13 19:20:04 +0200

    feat: add cors permissive policy

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod url_repository;
 use app::AppState;
 use routes::{add_url, check_id, health_check, not_found, redirect_to_target};
 use telemetry::{get_trace_layer, init_tracing_subscriber};
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
@@ -28,6 +29,7 @@ async fn main() {
         .route("/add", post(add_url))
         .route("/check", post(check_id))
         .layer(trace_layer)
+        .layer(CorsLayer::permissive()) // FIXME: use better CORS policy
         .with_state(app_state);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
