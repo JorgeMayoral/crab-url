c7cb0c98592770e16157acf0354868f46860cd3b
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2023-08-30 17:42:18 +0200

    feat: remove unused dependencies

--- src/url_repository.rs
+++ src/url_repository.rs
@@ -8,7 +8,6 @@ pub struct UrlRepository {
 
 impl UrlRepository {
     pub fn new() -> Self {
-        dotenvy::dotenv().expect("Failed to load .env file"); // TODO: Find a better way to do this
         let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL env var not set");
         let redis_client = Client::open(redis_url).expect("Failed to connect to redis");
         Self {
