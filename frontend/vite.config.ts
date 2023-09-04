80d42af48beeeffc4a4e4d379dc2b1f5e6b3d12f
Jorge Mayoral Alvarez <jorge.ma@yorch.dev>
2023-09-04 19:02:05 +0200

    feat: add dev proxy (#6)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/add': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      '/check': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    }
+  }
 })
